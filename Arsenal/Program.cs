7b7dd6523d659da097fc3cf15dd2a3ef4db0d3f6
Samuel Skoog <samule.skoog@elev.ga.ntig.se>
2024-10-11 13:01:08 +0200

    jhvuyv

---
+++ Arsenal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+string contents = File.ReadAllText("weaponl.json");
+
+Weapon w = JsonSerializer.Deserialize<Weapon>(contents);
+
+Console.WriteLine("Hur många attacker?");
+
+int numAttacks = 0;
+
+string numAttacksStr = "";
+
+while (!int.TryParse(numAttacksStr, out numAttacks))
+{
+    numAttacksStr = Console.ReadLine();
+}
+
+int damageSum = 0;
+
+Console.ForegroundColor = ConsoleColor.Red;
+
+for (int i = 0; i < numAttacks; i++)
+{
+    int damage = w.Attack();
+    Console.WriteLine($"+{damage}");
+    damageSum += damage;
+}
+
